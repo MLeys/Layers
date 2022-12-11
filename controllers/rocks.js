63c9909a07b26e1a03b082aa91ea5803eb5202a9
username <mleyspainting@gmail.com>
2022-12-10 23:16:24 -0500

    minor html and reordering create populates

--- controllers/rocks.js
+++ controllers/rocks.js
@@ -9,6 +9,8 @@ module.exports = {
 
 async function deleteRock(req, res) {
     try {
+        console.log(req.params.id, '++++++++++++++++++++ req.params.id')
+        console.log('DELETE ROUTE &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
         const rockDoc = await Rock.findByIdAndDelete(req.params.id);
 
         res.redirect(`/projects/${rockDoc.projectId}`)
@@ -20,18 +22,14 @@ async function deleteRock(req, res) {
 
 async function create(req, res) {
     try {    
-        const projectDoc = await Project.findById(req.params.id)
-            .populate("userCreated")
-            .populate("usersAssigned")
-            .populate("rocks")
-            .exec();
-        req.body.userId = req.user._id;
-        req.body.userName = req.user.name;
-        req.body.userAvatar = req.user.avatar;
-        req.body.projectId = projectDoc._id;
+        const projectDoc = await Project.findById(req.params.id).exec();
+            req.body.userId = req.user._id;
+            req.body.userName = req.user.name;
+            req.body.userAvatar = req.user.avatar;
+            req.body.projectId = projectDoc._id;
 
-        console.log(req.params, '++++++++++++++++++++ req.params')
-        console.log(projectDoc._id, '---------------------projectdoc._id')
+        console.log(req.params.id, '++++++++++++++++++++ req.params.id')
+        console.log(req.body._id, '---------------------req.body._id')
         
         const newRock = await Rock.create(req.body);
 
@@ -39,21 +37,22 @@ async function create(req, res) {
             .populate("projectId")
             .populate("userId")
             .exec();
+            projectDoc.rocks.push(rockDoc);
+            Project.findById(req.params.id)
+                .populate("userCreated")
+                .populate("usersAssigned")
+                .populate("rocks")
+                .exec();
+
+            projectDoc.save(function(err) {            
+
+                res.redirect(`/projects/${ req.params.id }`);
+                // res.redirect(`/projects/${ projectDoc.id }`);
+            })    
             rockDoc.save( function(err) {
                 if (err) return res.send('projectDoc SAve Error rocks controller create');
                 
-                projectDoc.rocks.push(rockDoc);
-                Project.findById(req.params.id)
-                    .populate("userCreated")
-                    .populate("usersAssigned")
-                    .populate("rocks")
-                    .exec();
-
-                projectDoc.save(function(err) {            
-
-                    res.redirect(`/projects/${ req.params.id }`);
-                    // res.redirect(`/projects/${ projectDoc.id }`);
-                })       
+   
             })        
     } catch(err) {
         console.log(err);
