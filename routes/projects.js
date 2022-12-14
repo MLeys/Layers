be28724ecca13e69dc7b725acefbd0bede0468e2
username <mleyspainting@gmail.com>
2022-12-14 05:43:06 -0500

    tryin to fix edit

--- routes/projects.js
+++ routes/projects.js
@@ -7,19 +7,18 @@ const isLoggedIn = require('../config/auth')
 router.get('/new', isLoggedIn, projectsCtrl.new);
 router.get('/', isLoggedIn, projectsCtrl.index);
 router.post('/', isLoggedIn, projectsCtrl.create);
-router.get('/all', isLoggedIn, projectsCtrl.all);
+
 
 router.get('/:id/edit', isLoggedIn, projectsCtrl.edit);
 
 router.put('/:id', isLoggedIn, projectsCtrl.update);
-
-
 
 router.delete('/:id', isLoggedIn, projectsCtrl.delete);
 router.post('/:id/unAssign', isLoggedIn, projectsCtrl.unAssign);
 
 router.post('/:id', isLoggedIn, projectsCtrl.add); 
 
+router.get('/all', isLoggedIn, projectsCtrl.all);
 // addAssigned - add logged in user to prroject
 router.get('/:id', isLoggedIn, projectsCtrl.show);
 
